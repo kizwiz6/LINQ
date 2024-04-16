f12ffab6fdfea6ed43844acfcb99ad402837a1fc
Kieran Emery <kizwiz@hotmail.co.uk>
2024-04-16 04:18:42 +0100

    LINQ with other collections

--- Program.cs
+++ Program.cs
@@ -27,6 +27,16 @@
                 Console.WriteLine(v);
             }
 
+
+            List<string> heroesList = new List<string> { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+            var heroesWithSpecialChars = heroesList.Where(h => h.Contains('.') || h.Contains('7'));
+
+            foreach (var v in heroesWithSpecialChars)
+            {
+                Console.WriteLine(v);
+            }
+
         }
     }
 }
